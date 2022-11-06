0145308b7a03506edefe4cc501ef4425fbaf7948
Matheus Santos <matheusgabriel.18@gmail.com>
2022-11-06 20:34:05 -0300

    feat: add initial html structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <main>
+    <h1>IP Address Tracker</h1>
+
+    <div class="">
+      <input type="text" v-model="ipInput" placeholder="Search for any IP address or domains" />
+    </div>
+
+    <section>
+      <ul>
+        <li>
+          <strong>IP Address</strong>
+          <span>-----</span>
+        </li>
+        <li>
+          <strong>Location</strong>
+          <span>-----</span>
+        </li>
+        <li>
+          <strong>Timezone</strong>
+          <span>UTC-----</span>
+        </li>
+        <li>
+          <strong>ISP</strong>
+          <span>-----</span>
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <script>
 export default {
   name: 'App',
-}
+
+  data() {
+    return {
+      ipInput: '',
+    };
+  },
+};
 </script>
 
 <style lang="scss">
@@ -14,4 +47,8 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+</style>
+
+<style lang="scss" scoped>
+
 </style>
